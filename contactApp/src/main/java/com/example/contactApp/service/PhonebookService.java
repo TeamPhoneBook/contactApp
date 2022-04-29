6ee9c077d76e3e309b5d27127c57ac99fe98bd6a
Chelsea Cole <73906280+CcoleCode@users.noreply.github.com>
2022-04-29 13:21:09 -0400

    service class re-commit

--- contactApp/src/main/java/com/example/contactApp/service/PhonebookService.java
+++ contactApp/src/main/java/com/example/contactApp/service/PhonebookService.java
@@ -46,33 +46,11 @@ public Contact updateContacts(Long Id, Contact contactsDetails) {
      contacts.setName(contactsDetails.getName());
      contacts.setPhoneNumber(contactsDetails.getPhoneNumber());
      contacts.setCreatedAt(contactsDetails.getCreatedAt());
+     contacts.setEmail(contactsDetails.getEmail());
+     contacts.setDob(contactsDetails.getDob());
+     contacts.setWebsite(contactsDetails.getWebsite());
      
      return contactsRepository.save(contacts);                                
 }
 
-//UPDATE PERSON
-public Contact updatePerson(Long Id, PersonModel contactsDetails) {
-    PersonModel person = (PersonModel) contactsRepository.findById(Id).get();
-    person.setName(contactsDetails.getName());
-    person.setPhoneNumber(contactsDetails.getPhoneNumber());
-    person.setCreatedAt(contactsDetails.getCreatedAt());
-    person.setEmail(contactsDetails.getEmail());
-    person.setDob(contactsDetails.getDob());
-    
-    return contactsRepository.save(person);             
-
-
-}
-
-//UPDATE ORGANIZATION
-public Contact updateOrganization(Long Id, OrganizationModel contactsDetails) {
-	OrganizationModel organization = (OrganizationModel) contactsRepository.findById(Id).get();
-	organization.setName(contactsDetails.getName());
-	organization.setPhoneNumber(contactsDetails.getPhoneNumber());
-	organization.setCreatedAt(contactsDetails.getCreatedAt());
-	organization.setWebsite(contactsDetails.getWebsite());
-    
-    return contactsRepository.save(organization); 
-    
-}
 }
