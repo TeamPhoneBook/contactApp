18d25b85daef1b6b99e21d277bc89637733416bd
harmerr <101577717+harmerr@users.noreply.github.com>
2022-04-29 14:04:12 -0400

    Controller, Service, and application.properties

--- contactApp/src/main/java/com/example/contactApp/controller/PhoneBookController.java
+++ contactApp/src/main/java/com/example/contactApp/controller/PhoneBookController.java
@@ -1,87 +1,69 @@
 package com.example.contactApp.controller;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.example.Music.model.Music;
-import com.example.contactApp.model.Employee;
+import com.example.contactApp.model.Contact;
 import com.example.contactApp.model.OrganizationModel;
 import com.example.contactApp.model.PersonModel;
 import com.example.contactApp.service.PhonebookService;
 
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/contacts")
 public class PhoneBookController {
 	 @Autowired
      PhonebookService phoneService;
 
-	//CREATE PERSON ENTRY
-     @PostMapping("/contacts/persons")
-     public OrganizationModel createPerson(@RequestBody PersonModel personDetails) {
-         return phoneService.createOrganization(personDetails);
+	 //CREATE PERSON ENTRY
+     @PostMapping("/persons")
+     public Contact createPerson(@RequestBody PersonModel person) {
+    	 return phoneService.createPerson(person);
      }
      
-	 //CREATE ORGANIZATION ENTRY
-     @PostMapping("/contacts/organizations")
-     public OrganizationModel createOrganization(@RequestBody OrganizationModel orgDetails) {
-         return phoneService.createEmployee(emp);
+     //CREATE ORGANIZATION ENTRY
+     @PostMapping("/organization")
+     public Contact createOrganization(@RequestBody OrganizationModel organization) {
+    	 return phoneService.createOrganization(organization);
      }
      
-     
-     @GetMapping("/contacts")
+     //READ CONTACTS
+     @GetMapping("/all")
      public List<Contact> readContacts() {
-         return phoneService.getEmployees();
+         return phoneService.getContacts();
      }
      
-     //GET PERSONS
-     @GetMapping("/contacts/persons")
-     public List<PersonModel> readPersons() {
-         return phoneService.getTracks();
+     //UPDATE PERSONS
+     @PutMapping("/persons")
+     public PersonModel updatePersons(Long id, PersonModel personsInfo) {
+    	 return phoneService.updatePersons(id, personsInfo);
      }
      
-     //GET ORGANIZATIONS
-     @GetMapping("/contacts/organizations")
-     public List<OrganizationModel> readOrganizations() {
-         return phoneService.getTracks();
-     }
-     
-     //GET ORGANIZATIONS
-     @GetMapping("/contacts/organizations")
-     public Optional<Music> readTrack(@PathVariable(value="id") Long id) {
-     	return phone.getTrack();
-     }
-     
-     //PATCH
-     @PatchMapping("/music/{id}/{name}")
-     public Music patchTrack(@PathVariable(value = "id") Long id, @PathVariable(value = "name") String name) {
-     	return musicService.patchTrack(id, name);
-     }
-
-     //PUT
-     @PutMapping("/music/{id}")
-     public Music updateTrack(@PathVariable(value = "id") Long id, @RequestBody Music trackDetails) {
-     	return musicService.updateTrack(id, trackDetails);
+     //UPDATE ORGANIZATIONS
+     @PutMapping("/organizations")
+     public OrganizationModel updateContacts(Long id, OrganizationModel orgInfo) {
+    	 return phoneService.updateOrganizations(id, orgInfo);
      }
      
      //DELETE
-     @DeleteMapping("/music/{id}")
-     public void deleteTrack(@PathVariable(value = "id") Long id) {
-     	musicService.deleteTrack(id);
+     @DeleteMapping("/all")
+     public void deleteTrack(Long id) {
+     	phoneService.deleteContacts(id);
      }
+     
+     //PATCH
+     //@PatchMapping("/music/{id}/{name}")
+     //public Music patchTrack(@PathVariable(value = "id") Long id, @PathVariable(value = "name") String name) {
+     	//return musicService.patchTrack(id, name);
+     //}
+
+     
 
 }
